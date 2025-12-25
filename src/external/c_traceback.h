e2d6da1521a3cf137b9220ab4cab170feda3a043
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-12-25 16:32:51 +0800

    Update inline printing functions API and add error inline function

--- src/external/c_traceback.h
+++ src/external/c_traceback.h
@@ -17,39 +17,75 @@ typedef struct CTB_Context CTB_Context;
 #define MAX_TRACEBACK_FRAMES 64
 
 /**
+ * \brief Wrapper for printing an inline error.
+ *
+ * \param[in] error_code Error code associated with the error.
+ * \param[in] msg Error message.
+ */
+#define CTB_PRINT_INLINE_ERROR(error_code, msg)                                        \
+    do                                                                                 \
+    {                                                                                  \
+        ctb_print_inline_error(__FILE__, __LINE__, __func__, error_code, msg);         \
+    } while (0)
+
+/**
  * \brief Wrapper for printing an inline warning.
  *
  * \param[in] error_code Error code associated with the warning.
- * \param[in] warning_msg Warning message.
+ * \param[in] msg Warning message.
  */
-#define CTB_PRINT_WARNING(error_code, warning_msg)                                     \
-    ctb_print_warning(__FILE__, __LINE__, __func__, error_code, warning_msg)
+#define CTB_PRINT_INLINE_WARNING(error_code, msg)                                      \
+    do                                                                                 \
+    {                                                                                  \
+        ctb_print_inline_warning(__FILE__, __LINE__, __func__, error_code, msg);       \
+    } while (0)
 
 /**
  * \brief Wrapper for printing an inline message.
  *
  * \param[in] msg Warning message.
  */
-#define CTB_PRINT_MESSAGE(msg) ctb_print_message(__FILE__, __LINE__, __func__, msg)
+#define CTB_PRINT_INLINE_MESSAGE(msg)                                                  \
+    do                                                                                 \
+    {                                                                                  \
+        ctb_print_inline_message(__FILE__, __LINE__, __func__, msg);                   \
+    } while (0)
 
 CTB_Context *ctb_make_context(void);
 void ctb_free_context(CTB_Context *restrict context);
 
 /**
- * \brief Print error message to stderr.
+ * \brief Print inline error message to stderr.
  *
- * \param[in] warning_file File where the warning occurs.
- * \param[in] warning_line Line number where the warning occurs.
- * \param[in] warning_func Function where the warning occurs.
- * \param[in] error_code Error code associated with the warning.
- * \param[in] warning_msg Warning message.
+ * \param[in] file File where the error occurs.
+ * \param[in] line Line number where the error occurs.
+ * \param[in] func Function where the error occurs.
+ * \param[in] error_code Error code associated with the error.
+ * \param[in] msg Error message.
  */
-void ctb_print_warning(
-    const char *restrict warning_file,
-    const int warning_line,
-    const char *restrict warning_func,
+void ctb_print_inline_error(
+    const char *restrict file,
+    const int line,
+    const char *restrict func,
     const int error_code,
-    const char *restrict warning_msg
+    const char *restrict msg
+);
+
+/**
+ * \brief Print inline warning message to stderr.
+ *
+ * \param[in] file File where the warning occurs.
+ * \param[in] line Line number where the warning occurs.
+ * \param[in] func Function where the warning occurs.
+ * \param[in] error_code Error code associated with the warning.
+ * \param[in] msg Warning message.
+ */
+void ctb_print_inline_warning(
+    const char *restrict file,
+    const int line,
+    const char *restrict func,
+    const int error_code,
+    const char *restrict msg
 );
 
 /**
@@ -60,7 +96,7 @@ void ctb_print_warning(
  * \param[in] func Function where the message is sent.
  * \param[in] msg Message.
  */
-void ctb_print_message(
+void ctb_print_inline_message(
     const char *restrict file,
     const int line,
     const char *restrict func,
